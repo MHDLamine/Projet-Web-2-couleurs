0080625b6549c0aa296287c396584957b4e252f7
Mouhamed Lamine NGOM <mouhamed.ngom50@gmail.com>
2022-08-27 00:30:26 +0000

    Finish

--- script.js
+++ script.js
@@ -11,3 +11,36 @@ function play()
     }
     
 }
+
+
+var buttonSend = document.getElementById("submit");
+var tabUsers = [];
+buttonSend.addEventListener("click", Afficher);
+
+function Afficher()
+ {
+   var list = document.getElementById("list");
+   var prenom = document.getElementById("username").value;
+   var nom = document.getElementById("name").value;
+   var mail = document.getElementById("mail").value;
+   var phone = document.getElementById("phone").value;
+   var message = document.getElementById("message").value;
+
+   list.innerHTML = "<p>Prenom :"+ prenom +"</p>"+"<p>Nom :"+ nom +"</p>"+"<p>Adresse mail:"
+                    + mail +"</p>"
+                    +"<p>Numéro: "+ phone +"</p>"+"<p>Message: "+ message +"</p>";
+    
+    var objUsers = 
+    {
+        PRENOM : prenom,
+        NOM: nom,
+        MAIL: mail,
+        PHONE: phone,
+        MESSAGE : message,
+
+    }
+  
+   tabUsers.push(objUsers);
+   console.log(tabUsers)
+
+}
